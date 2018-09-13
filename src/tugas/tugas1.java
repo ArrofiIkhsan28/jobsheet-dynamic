d6c6eaeef4854e1ac84f0c03289d2454c8f0286d
Hp <Hp@DESKTOP-GFENN21>
2018-09-13 13:44:16 +0700

    initial commit

---
+++ src/tugas/tugas1.java
@@ -0,0 +1,15 @@
+
+package tugas;
+public class tugas1 {
+     private static void tampiljudul(String identitas){
+     System.out.println(identitas +"identitas");
+     
+     System.out.println("\nhitung fibonnaci");
+     System.out.println("1,1,2,3,5,8,13....dst.\n");
+ }
+ public static void main(String[]args){
+      
+     String identitas = (" arrofi ikhsan nur abror / xrpl2/08");
+     tampiljudul(identitas);
+ }
+}
