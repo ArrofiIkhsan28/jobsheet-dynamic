f384c23e061fab86d46c64099a9f25646064b6bf
Hp <Hp@DESKTOP-GFENN21>
2018-09-15 06:15:16 +0700

    initial commit

---
+++ src/ORETAN1/tug1.java
@@ -0,0 +1,13 @@
+package ORETAN1;
+public class tug1 {
+private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("100, 500, 1000, 2000, 5000\n");
+    }
+  public static void main(String[] args) {
+        String identitas = "arrofi ikhsan nur abror / XRPL2 / 08";
+        
+        tampilJudul(identitas);}
+}
